36aa5710b254806e1867e09c9e2ac789a2eb54f7
Kuroashi <kuroashi.prog@gmail.com>
2022-01-28 15:12:14 -0300

    mejorada la calculadora de figuras en una funcion

--- figuras.js
+++ figuras.js
@@ -1,6 +1,35 @@
-//inicicializando variables para las figuras geometricas
+//creando una sola funcion para calcular los perímetros y areas
 
-//cuadrado
+
+const PI = Math.PI;
+var lado1, lado2, base, fiugra;
+
+console.log("Inicialice la función calculos(figura, lado1 o radio, lado2, base, altura), recibe Cuadrado, Triangulo y Circulo");
+
+function calculos(figura, lado1, lado2, base, altura) {
+    switch(figura) {
+        case "Cuadrado":
+            var perimetro = lado1 * 4;
+            var area = lado1 * lado1;
+            console.log(`Para este ${figura}, su perímetro es de ${perimetro}cm y su área es de ${area} cm^2`);
+            break;
+        case "Triangulo":
+            var perimetro = lado1 + lado2 + base;
+            var area = (base * altura) / 2;
+            console.log(`Para este ${figura}, su perímetro es de ${perimetro}cm y su área es de ${area} cm^2`);
+            break;
+        case "Circulo":
+            var perimetro = 2 * PI * lado1;
+            var area = PI * lado1 * lado1;
+            console.log(`Para este ${figura}, su perímetro es de ${perimetro}cm y su área es de ${area} cm^2`);
+            break;
+        default:
+            console.log("La figura especificada no es una de las aceptadas")
+    }
+}
+            
+//inicicializando variables para las figuras geometricas
+/*/cuadrado
 console.group("Cuadrado");
 
 const ladoCuadrado = 5;
@@ -46,4 +75,4 @@ console.log("La circunferencia es " + circunferencia + "cm");
 var areaCirculo = PI * (diametroCirculo / 2) * (diametroCirculo / 2);
 console.log("el área del circulo es " + areaCirculo + "cm^2");
 
-console.groupEnd();
+console.groupEnd();*/
